f70ed8bfbfe33f6f1719d569d97d10e392948324
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-05-01 14:10:10 +0700

    fix: fix package export bug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,12 @@ module com.csym025_resit {
     requires javafx.fxml;
 
     opens com.csym025_resit to javafx.fxml;
+    opens com.csym025_resit.Controller.Customer to javafx.fxml;
+    opens com.csym025_resit.Controller.Rent to javafx.fxml;
+    opens com.csym025_resit.Controller.Stock to javafx.fxml;
+    
     exports com.csym025_resit;
+    exports com.csym025_resit.Controller.Customer;
+    exports com.csym025_resit.Controller.Rent;
+    exports com.csym025_resit.Controller.Stock;
 }
