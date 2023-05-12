f0e1fae1032546f91888bd312486d9ec5bb6b2a4
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-05-13 03:57:44 +0700

    feat: add add customer feature

--- src/main/java/com/csym025_resit/AddCustomerController.java
+++ src/main/java/com/csym025_resit/AddCustomerController.java
@@ -1,6 +1,15 @@
 package com.csym025_resit;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.UUID;
+
+import com.csym025_resit.Model.Customer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,12 +18,36 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AddCustomerController {
 
     @FXML
     private Button customerLinkButton;
+
+    @FXML
+    private TextField fullNameInput;
+
+    @FXML
+    private TextField emailInput;
+
+    @FXML
+    private TextField phoneInput;
+
+    @FXML
+    private TextArea addressInput;
+
+    @FXML
+    private RadioButton genderMaleInput;
+
+    @FXML
+    private RadioButton genderFemaleInput;
+    
+    @FXML
+    private Button backButton;
 
     private Stage stage;
     private Scene scene;
@@ -42,5 +75,47 @@ public class AddCustomerController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void addCustomer() throws IOException, ClassNotFoundException {
+        Customer customer = new Customer();
+        String pathname = "src/main/java/com/csym025_resit/Serialization/Customer.ser";
+        customer.id = UUID.randomUUID().toString();
+        customer.fullName = fullNameInput.getText();
+        customer.email = emailInput.getText();
+        customer.phone = phoneInput.getText();
+        customer.address = addressInput.getText();
+        if (genderMaleInput.getText() == "Male") {
+            customer.gender = "Male";
+        } else {
+            customer.gender = "Female";
+        }
+
+        File f = new File(pathname);
+        if (f.exists()) {
+            Customer[] customers = null;
+            FileInputStream fileIn = new FileInputStream(pathname);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            customers = (Customer[]) in.readObject();
+            in.close();
+            fileIn.close();
+
+            Customer[] newCustomers = Arrays.copyOf(customers, customers.length + 1);
+            newCustomers[customers.length] = customer;
+
+            FileOutputStream fileOut = new FileOutputStream(pathname);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(newCustomers);
+            out.close();
+            fileOut.close();
+        } else {
+            FileOutputStream fileOut = new FileOutputStream(pathname);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(new Customer[] { customer });
+            out.close();
+            fileOut.close();
+        }
+
+        backButton.fire();
     }
 }
---
+++ src/main/java/com/csym025_resit/Model/Customer.java
@@ -0,0 +1,12 @@
+package com.csym025_resit.Model;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    public String id;
+    public String fullName;
+    public String email;
+    public String phone;
+    public String address;
+    public String gender;
+}
