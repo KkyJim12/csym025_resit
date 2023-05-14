ad8df4369bb5b85ba987553cacc625b97f2842ef
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-05-15 02:49:07 +0700

    feat: add store invoice feature

--- src/main/java/com/csym025_resit/AddRentingController.java
+++ src/main/java/com/csym025_resit/AddRentingController.java
@@ -7,10 +7,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.UUID;
 
 import com.csym025_resit.Model.Cart;
 import com.csym025_resit.Model.Customer;
+import com.csym025_resit.Model.Invoice;
 import com.csym025_resit.Model.Stock;
 
 import javafx.event.ActionEvent;
@@ -45,11 +48,13 @@ public class AddRentingController {
     private ScrollPane showArea;
 
     @FXML
-    private ChoiceBox<Object> customerInput;
+    private ChoiceBox<String> customerInput;
     @FXML
     private ChoiceBox<String> productInput;
     @FXML
     private TextField qtyInput;
+    @FXML
+    private Button backButton;
 
     @FXML
     public void initialize() throws IOException, ClassNotFoundException {
@@ -103,7 +108,7 @@ public class AddRentingController {
 
             for (int i = 0; i < customers.length; i++) {
                 customerInput.getItems().add(customers[i].fullName);
-                customerInput.setValue(customers[i]);
+                customerInput.setValue(customers[i].fullName);
             }
 
             customerInput.getSelectionModel().select(0);
@@ -204,9 +209,8 @@ public class AddRentingController {
             out.close();
             fileOut.close();
         }
-    }
 
-    public void removeProductFromCart() {
+        getCart();
     }
 
     public void getCart() throws IOException, ClassNotFoundException, FileNotFoundException {
@@ -239,21 +243,14 @@ public class AddRentingController {
                         "Total Price: " + Integer.toString(cart[i].pricePerDay * cart[i].quantity));
                 totalPrice.setStyle("-fx-font-size:15; -fx-font-family: Segoe UI");
                 totalPrice.setTextFill(Color.color(1, 1, 1));
-                Button deleteButton = new Button("Delete");
-                deleteButton.setStyle("-fx-font-size:15; -fx-font-family: Segoe UI; -fx-background-color:#f97316");
-                deleteButton.setTextFill(Color.color(1, 1, 1));
-                deleteButton.setId(cart[i].productName);
                 // deleteButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new
                 // removeProductFromCart());
-                HBox manageSection = new HBox(deleteButton);
-                manageSection.setSpacing(10);
 
                 VBox productCard = new VBox();
                 productCard.getChildren().add(productName);
                 productCard.getChildren().add(category);
                 productCard.getChildren().add(quantity);
                 productCard.getChildren().add(totalPrice);
-                productCard.getChildren().add(manageSection);
 
                 productCard.setPrefSize(425, 150);
                 productCard.setPadding(new Insets(10, 20, 10, 20));
@@ -265,6 +262,67 @@ public class AddRentingController {
 
         showArea.setContent(grid);
         showArea.setPannable(true);
+    }
+
+    public void addInvoice() throws IOException, ClassNotFoundException, FileNotFoundException {
+        int totalPrice = 0;
+        int discount = 0;
+        int lastPrice = 0;
+        Cart[] cart = null;
+
+        String pathname = "src/main/java/com/csym025_resit/Serialization/Cart.ser";
+        File f = new File(pathname);
+        if (f.exists()) {
+            FileInputStream fileIn = new FileInputStream(pathname);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            cart = (Cart[]) in.readObject();
+            in.close();
+            fileIn.close();
+
+            for (int i = 0; i < cart.length; i++) {
+                totalPrice += cart[i].pricePerDay * cart[i].quantity;
+            }
+
+            System.out.println(totalPrice);
+        }
+
+        Invoice invoice = new Invoice();
+        String pathname1 = "src/main/java/com/csym025_resit/Serialization/Invoice.ser";
+        invoice.id = UUID.randomUUID().toString();
+        invoice.customerName = customerInput.getValue();
+        invoice.cart = cart;
+        invoice.totalPrice = totalPrice;
+        invoice.discount = discount;
+        invoice.lastPrice = lastPrice;
+        invoice.rentDate = LocalDateTime.now();
+        invoice.returnDate = null;
+
+        File f1 = new File(pathname1);
+        if (f1.exists()) {
+            Invoice[] invoices = null;
+            FileInputStream fileIn = new FileInputStream(pathname1);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            invoices = (Invoice[]) in.readObject();
+            in.close();
+            fileIn.close();
+
+            Invoice[] newInvoices = Arrays.copyOf(invoices, invoices.length + 1);
+            newInvoices[invoices.length] = invoice;
+
+            FileOutputStream fileOut = new FileOutputStream(pathname1);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(newInvoices);
+            out.close();
+            fileOut.close();
+        } else {
+            FileOutputStream fileOut = new FileOutputStream(pathname1);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(new Invoice[] { invoice });
+            out.close();
+            fileOut.close();
+        }
+
+        backButton.fire();
     }
 
 }
---
+++ src/main/java/com/csym025_resit/Model/Invoice.java
@@ -0,0 +1,15 @@
+package com.csym025_resit.Model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Invoice implements Serializable {
+    public String id;
+    public String customerName;
+    public Cart[] cart;
+    public int totalPrice;
+    public int discount;
+    public int lastPrice;
+    public LocalDateTime rentDate;
+    public LocalDateTime returnDate;
+}
