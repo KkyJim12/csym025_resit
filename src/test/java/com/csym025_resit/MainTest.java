3313ab4d42dba2224f4e1470033615d1694267d0
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-05-16 22:41:40 +0700

    feat: add gitignore and example test

--- src/test/java/com/csym025_resit/MainTest.java
+++ src/test/java/com/csym025_resit/MainTest.java
@@ -1,4 +1,14 @@
+package com.csym025_resit;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @Test
+    public void abc() {
+        int x = 0;
+        assertEquals(0, x);
+    }
+
 }
